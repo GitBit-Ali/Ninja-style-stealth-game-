f6d362b838660c08a530e5a736daf8a1b2b5874d
Ali Morgan <alimorgan1527@gmail.com>
2022-05-14 19:36:38 +0200

    الحمد لله

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : class
+{
+    public static T instance;
+
+    private void Awake ()
+    {
+        T thisT = this as T;
+        if (instance == null)
+        {
+            instance = thisT;
+        }
+        else if (instance != thisT && instance != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
